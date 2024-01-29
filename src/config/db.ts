512e9fd3393b4eed4428c3d45d8480370cee12c3
Cristopher <38367188+iwishiwasatoy@users.noreply.github.com>
2024-01-29 08:49:44 -0400

    configure db connection function

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,12 @@
 import mongoose from 'mongoose'
+import { configDotenv } from 'dotenv'
+configDotenv()
+
+const DB_URI: string = process.env.DB_URI!
+
+export async function connectToDB (): Promise<void> {
+  console.log(DB_URI)
+  await mongoose.connect(DB_URI)
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'mongo connection error'))
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import { connectToDB } from './config/db'
 const PORT = 3000
-
 const app = express()
+void connectToDB()
+
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'This is the index' })
