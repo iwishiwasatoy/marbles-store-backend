b85492b08b24488210a7d2318d03a30bf7a6436b
Cristopher <38367188+iwishiwasatoy@users.noreply.github.com>
2024-01-29 14:04:52 -0400

    fix: app.get -> app.use trying to use the router with the middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import { connectToDB } from './config/db'
-const PORT = 3000
+import collectorRouter from './routes/collectorRouter'
+const PORT = process.env.PORT ?? 3000
 const app = express()
 void connectToDB()
 
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.json({ message: 'This is the index' })
 })
+
+app.use('/api/v1/collectors', collectorRouter)
 
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) })
